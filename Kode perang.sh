5de0b5151f0e9ff0959d71abcfae9688ec24aebc
TEAMAHS <67921135+TEAMAHS@users.noreply.github.com>
2020-07-10 01:05:39 +0700

    Update Kode perang.sh

--- Kode perang.sh
+++ Kode perang.sh
@@ -17,7 +17,8 @@ echo " 04. Spam chat wa" | lolcat
 echo " 05. Hack fb" | lolcat
 echo " 06. Cara sadap wa" | lolcat
 echo " 07. Hack Gmail" | lolcat
-echo " 08. Spam gmail" | lolcat
+echo " 08. Virus Trojan" | lolcat
+echo " 09. Munculkan tools tambahan untuk mempermudah" | lolcat
 echo " Ketik Nomor  : " | lolcat
 read nomor
 
@@ -63,4 +64,71 @@ pkg install git -y
 git clone https://github.com/siputra12/prank.git
 cd prank
 php wa.php
+fi
+
+if [ $nomor = 5 ] || [ $nomor = 05 ]
+then
+clear
+echo " Install Hack fb.. " | lolcat
+apt update && apt upgrade
+apt install python -y
+apt install python2 -y
+apt install ruby -y
+apt install git -y
+apt install wget -y
+apt install curl -y 
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/hnov7/mbf
+cd mbf
+python2 mbf.py
+fi
+
+if [ $nomor = 6 ] || [ $nomor = 06 ]
+then
+clear
+echo " Install Cara sadap wa.. " | lolcat
+pkg install git
+pkg install curl -y
+git clone https:// github.com/AndriGanz/whatshack
+cd whatshack
+ls
+sh whatshack.sh
+fi
+
+if [ $nomor = 7 ] || [ $nomor = 07 ]
+then
+clear
+echo " Install Hack Gmail.. " | lolcat
+apt-get update && apt-get upgrade
+apt-get install git -y
+apt-get install python python-pip python-setuptools -y
+pip install scapy
+git clone https://github.com/wifiphisher/wifiphisher.git
+cd wifiphisher<
+python setup.py install
+cd wifiphisher
+python wifiphisher
+fi
+
+if [ $nomor = 8 ] || [ $nomor = 08 ]
+then 
+clear
+echo " Virus Trojan.. " | lolcat
+pkg install git
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/R133F/Trojans
+cd trojans
+fi
+
+if [ $nomor = 9 ] || [ $ nomor 09 ]
+then
+clear
+echo " Install Munculkan tools tambahan untuk mempermudah.. " | lolcat
+pkg install python -y
+pkg install git
+git clone https://github.com/kumpulanremaja/termuxtbb
+cd termuxtbb
+python kumpulanremaja.py
 fi
